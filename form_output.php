0e0c822c8b0f7bc42094057f6c952a44e9328220
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-04-12 14:56:11 +0200

    [add] form output page with text with and without the badword

--- form_output.php
+++ form_output.php
@@ -1,3 +1,10 @@
+<?php 
+$text = $_POST['inputText'];
+$badword = $_POST['inputBadWord'];
+
+$text_without_badword = str_replace($badword, '***',$text);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,10 +18,22 @@
       integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
       crossorigin="anonymous"
     />
-    
+
   <title>PHP Badwords</title>
 </head>
+
 <body>
- <h1>Prova</h1>
+ <div class="container my-5">
+
+  <h2 class="text-primary text-center">Testo completo</h2>
+  <p><span class="text-decoration-underline text-primary">Contenuto del testo:</span> <?php echo $text?></p>
+  <p><span class="text-decoration-underline text-primary">Lunghezza del testo:</span> <?php echo strlen($text)?> caratteri</p>
+
+  <h2 class="text-primary text-center">Testo senza badword</h2>
+  <p><span class="text-decoration-underline text-primary">Contenuto del testo:</span> <?php echo $text_without_badword?></p>
+  <p><span class="text-decoration-underline text-primary">Lunghezza del testo:</span> <?php echo strlen($text_without_badword)?> caratteri</p>
+  
+
+ </div>
 </body>
 </html>
